8852e72188803e74373ae165a65ecc228f28bb60
Zonimi <alex.zonimi@gmail.com>
2020-07-21 19:34:54 +0000

    edit

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Verifica sudo
 if command -V sudo > /dev/null 2>&1; then
     sudo_found="yes"
@@ -23,4 +24,23 @@ else
     fi
 fi
 
-echo "work"
+# Instala repositorios necessarios
+${sudo_cmd}apt install sed
+
+codename="$(cat /etc/lsb-release |grep CODENAME |awk -F = {'print $2'})"
+core=$(printf '%-1s' "$(grep -c cpu[0-9] /proc/stat)")
+
+# Instalar última versão nginx
+${sudo_cmd}echo "deb [arch=amd64] http://nginx.org/packages/mainline/ubuntu/ ${codename} nginx" > /etc/apt/sources.list.d/nginx.list
+${sudo_cmd}echo "deb-src http://nginx.org/packages/mainline/ubuntu/ ${codename} nginx" >> /etc/apt/sources.list.d/nginx.list
+${sudo_cmd}wget http://nginx.org/keys/nginx_signing.key
+${sudo_cmd}apt-key add nginx_signing.key
+${sudo_cmd}apt update && sudo apt -y upgrade
+${sudo_cmd}apt install nginx
+${sudo_cmd}systemctl start nginx
+${sudo_cmd}systemctl enable nginx
+${sudo_cmd}sed -i "1 s/nginx/www-data/" /etc/nginx/nginx.conf
+${sudo_cmd}sed -i "2 s/auto/$core/" /etc/nginx/nginx.conf
+${sudo_cmd}sed -i "13i\ \ \ \ \ \ \  server_tokens off;" /etc/nginx/nginx.conf
+${sudo_cmd}sed -i "14i\ " /etc/nginx/nginx.conf
+
